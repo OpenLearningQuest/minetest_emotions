ad3954b8c96b104ace54e1162c1bb00195068659
Ironmanhim3 <a.resende.victor@gmail.com>
2024-02-23 17:42:02 +0200

    Created a source for public use

---
+++ init.lua
@@ -0,0 +1,30 @@
+-- Happy block
+minetest.register_node("happymod:happy_block", {
+    description = "I'm happy!!!",
+    tiles = {"happyface3.png"},
+    groups = {cracky = 3, stone = 1},
+})
+-- Sad block
+minetest.register_node("happymod:sad_block", {
+    description = "I'm sad!!!",
+    tiles = {"sadface.png"},
+    groups = {cracky = 3, stone = 1},
+})
+-- Angry block
+minetest.register_node("happymod:angry_block", {
+    description = "I'm angry!!!",
+    tiles = {"angryface.png"},
+    groups = {cracky = 3, stone = 1},
+})
+-- augh block
+minetest.register_node("happymod:augh_block", {
+    description = "aughhhhh!!!",
+    tiles = {"augh.png"},
+    groups = {cracky = 3, stone = 1},
+})
+-- ooh block
+minetest.register_node("happymod:ooh_block", {
+    description = "Ooooooh shinyy!!!",
+    tiles = {"ooh.png"},
+    groups = {cracky = 3, stone = 1},
+})
